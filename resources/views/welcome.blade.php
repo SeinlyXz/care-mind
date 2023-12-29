be29e1de78356ec21a21b5569583668f928f780b
Seinly <macbookpro@MacBooks-MacBook-Pro.local>
2023-12-29 18:21:47 +0700

    Added Some Change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,10 +13,10 @@
     <div class="h-screen flex items-center justify-center">
         <div class="shadow-xl rounded-2xl">
             <div class="bg-white px-48 py-36 rounded-2xl grid grid-cols-1 grid-flow-row gap-y-10">
-                <h1 class="text-2xl font-semibold">Kamu Butuh Bantuan Darurat?</h1>
+                <h1 class="text-2xl font-semibold text-gray-500">Kamu Butuh Bantuan Darurat?</h1>
                 <div class="flex gap-3 justify-center">
-                    <a href="#" class="bg-teal-500 px-5 py-2 rounded-xl text-white text-xl">Iya</a>
-                    <a href="#" class="bg-teal-500 px-5 py-2 rounded-xl text-white text-xl">Tidak</a>
+                    <a href="#" class="bg-teal-500 px-[30px] py-2 rounded-xl text-white text-xl">Iya</a>
+                    <a href="#" class="bg-teal-500 px-[30px] py-2 rounded-xl text-white text-xl">Tidak</a>
                 </div>
             </div>
         </div>
