d544a3e9633f74d4ec1bd6f7078be0b5ee162097
winda <windaardelia@students.amikom.ac.id>
2024-01-11 21:43:51 +0700

    chat

--- resources/views/chatpsikolog.blade.php
+++ resources/views/chatpsikolog.blade.php
@@ -8,18 +8,14 @@
             </div>
             <br>
             <div class="ps-16">
-                <div class="bg-[#71B9C5] rounded-lg bg-opacity-25 w-96 h-28  ">
-                    <p class="text-white px-6 pt-3 text-2xl">Meditasi yang bisa dilakukan saat terpuruk</p>
+                <div class="bg-[#71B9C5] rounded-lg bg-opacity-25 w-96 h-96  ">
+                    <img class="w-80 px-5 " src="images/nana_gerhana.png" alt="">
+                    <br>
+                    <p class="text-white text-center">Nana Gerhana M.Psi, Psikolog, C.NN</p>
+                    <p class="text-white text-center">Nomor STR</p>
+                    <p class="text-white text-center">1224821203382202</p>
                 </div><br>
-                <div class="bg-[#71B9C5] rounded-lg bg-opacity-25 p-7 w-96 h-28 ">
-                    <p class="text-white -px-1 -pt-9 text-sm">"Bukanlah harta yang menjadikan hidup kaya, melainkan bagaimana kita memandangnya."
-                        ~Epicurus </p>
-                </div><br>
-                <div class="bg-[#71B9C5] rounded-lg bg-opacity-25 p-7 w-96 h-28">
-                    <p class="text-white -px-1 -pt-9 text-lg"> Merasa butuh penanganan lebih?</p>
-                    <button class="bg-[#2E5A61] text-white font-bold py-2 px-4 rounded-lg">Hubungi Psikolog</button>
-                </div><br>
-                <div class="bg-[#2E5A61] rounded-lg bg-opacity-25 p-7 w-96 h-40">
+                <div class="bg-[#2E5A61] rounded-lg bg-opacity-55 p-7 w-96 h-40">
                     <p class="text-white -px-1 -pt-9 text-lg"> Layanan Darurat </p>
                     <p class="text-white">Hotline </p>
                     <p class="text-white">>> 119 extension 8</p>
@@ -31,10 +27,10 @@
         <div class="top-2.5 width-[400px] height-[100px] w-[700px]">
             <div class="flex h-screen flex-col bg-gray-100 rounded-2xl">
                 <div class="bg-[#3B8A97] opacity-80 py-2 flex justify-items-start">
-                    <img class="w-24 px-2" src="images/oren-hi.png" alt="">
+                    <img class="w-20 px-2" src="images/nana_gerhana.png" alt="">
                     <div class=" flex flex-col justify my-auto">
-                        <h1 class="text-lg text-left text-white "> PAWSY</h1>
-                        <p class="  text-left text-white ">your friend </p>
+                        <h1 class="text-lg text-left text-white "> Nana Gerhana M.Psi, Psikolog, C.NN</h1>
+                        <p class="  text-left text-white ">your psikolog </p>
                     </div>
                 </div>
                 <div class="flex-grow overflow-y-auto">
